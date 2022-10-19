a0243c12122adf52bf860105bb422ed5baccfc0e
Euccreative <euccreative@gmail.com>
2022-10-19 22:20:56 +0100

    work

--- js/main.js
+++ js/main.js
@@ -5,3 +5,29 @@ faqs.forEach(faq =>{
             faq.classList.toggle('imgrotate')
     })
 })
+
+
+
+const navlink = document.querySelector('navlinks')
+const navlinka = document.querySelector('navlink a')
+const navopen = document.querySelector('open')
+const navclose = document.querySelector('close')
+
+
+function navopen(){
+    navlink.style.top ="40%";
+    navopen.style.display = "none"
+    navclose.style.display = "block"
+}
+
+function navclose(){
+    navlink.style.top ="-40%";
+    navopen.style.display = "block"
+    navclose.style.display = "none"
+}
+
+navlinka .forEach(navLink => {
+    navLink.addEventListener('click', function(){
+      closeNav()
+    })
+  })
